c8b5d403b2cb5142d05ce444070892fa8390b30d
Feramiz Bahaddinov <feramiz.bahadinnov@gmail.com>
2023-10-20 14:55:49 +0400

    footer added

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -2,50 +2,127 @@
 
 import React from 'react'
 import { MainLogo } from '@/app/public/assets/icons/icons'
+import {
+    TwitterIcon,
+    FacebookIcon,
+    LinkedinIcon,
+    InstagramIcon,
+} from '@/app/public/assets/icons/icons'
 
 const Footer = () => {
     return (
-        <footer className="flex mx-auto sticky   bottom-0 container   justify-center items-center">
-            <div className="flex">
-                <div className="">
-                    <MainLogo />
+        <section className="flex justify-center items-center   mx-auto absolute  bottom-0 w-full">
+            <footer className="container flex flex-col justify-center items-center  ">
+                <div className="flex justify-between w-full py-20">
+                    <div className="">
+                        <MainLogo />
+                    </div>
+                    <div className="grid grid-cols-3 gap-x-40 gap-y-9">
+                        <div className="flex flex-col text-4">
+                            <h1 className="font-[700] pb-4">SELL A HOME</h1>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Request an offer
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Buy
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Pricing
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Stories
+                            </span>
+                        </div>
+                        <div className="flex flex-col text-4">
+                            <h1 className="font-[700] pb-4">SELL A HOME</h1>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Request an offer
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Buy
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Pricing
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Stories
+                            </span>
+                        </div>
+                        <div className="flex flex-col text-4">
+                            <h1 className="font-[700] pb-4">SELL A HOME</h1>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Request an offer
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Buy
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Pricing
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Stories
+                            </span>
+                        </div>
+                        <div className="flex flex-col text-4">
+                            <h1 className="font-[700] pb-4">SELL A HOME</h1>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Request an offer
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Buy
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Pricing
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Stories
+                            </span>
+                        </div>
+                        <div className="flex flex-col text-4">
+                            <h1 className="font-[700] pb-4">SELL A HOME</h1>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Request an offer
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Buy
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Pricing
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Stories
+                            </span>
+                        </div>
+                        <div className="flex flex-col text-4">
+                            <h1 className="font-[700] pb-4">SELL A HOME</h1>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Request an offer
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Buy
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Pricing
+                            </span>
+                            <span className="text-[#000929] opacity-[0.7] font-[500]">
+                                Stories
+                            </span>
+                        </div>
+                    </div>
                 </div>
-                <div className="grid grid-cols-3 gap-x-2 gap-y-9">
-                    <div className=''>
-                        <h1 className='font-[700]'>SELL A HOME</h1>
-                        <span className='text-[#000929]'>Buy</span>
-                        <span className='text-[#000929]'>Buy</span>
+                <hr className="border-[1px] w-full absolute bottom-16 border-[#E8E6F9]" />
 
-                    </div>
-                    <div>
-                        <h1>SELL A HOME</h1>
-                        <span>Buy</span>
-                        <span>Builder trade-up</span>
-                        <span>Builder trade-up</span>
-                    </div>
-                    <div>
-                        <h1>SELL A HOME</h1>
-                        <span>Buy</span>
-                        <span>Builder trade-up</span>
-                        <span>Builder trade-up</span>
-                    </div>
-                    <div>
-                        <h1>SELL A HOME</h1>
-                        <span>Buy</span>
-                        <span>Builder trade-up</span>
-                        <span>Builder trade-up</span>
-                    </div>
-                    <div>
-                        <h1>SELL A HOME</h1>
-                        <span>Buy</span>
-                        <span>Builder trade-up</span>
-                        <span>Builder trade-up</span>
+                <div className="flex container justify-between ">
+                    <div className='opacity-[0.5] text-[#000929] font-[500]'>©2021 Estatery. All rights reserved</div>
+                    <div className="flex justify-between w-[216px] pb-5">
+                        <FacebookIcon className="opacity-[0.5]" />
+                        <TwitterIcon className="opacity-[0.5]" />
+                        <InstagramIcon className="opacity-[0.5]" />
+                        <LinkedinIcon className="opacity-[0.5]" />
                     </div>
                 </div>
-            </div>
-
-            <div></div>
-        </footer>
+            </footer>
+        </section>
     )
 }
 
